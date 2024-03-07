0b55ce484e326a3ada5bf5b68c74959712111635
+ <+>
2024-03-07 23:46:22 +0300

    Add signup , modify gitignore

--- server/config.py
+++ server/config.py
@@ -12,7 +12,7 @@ app = Flask(__name__) #instanciate a flask application
 app.secret_key = b'\xc2A\x1c\xc6\xc5QvJ?ZH$\x13\\4\xb0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['JWT_SECRET_KEY'] = 'ce81d8454bd966ba09bbbdf723f632fd'
 # app.json.compact = False
 jwt  = JWTManager(app)
 
